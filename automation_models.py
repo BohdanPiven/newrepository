342dc0690e1f4a362186214b9a27b19c46045e2c
Bohdan Piven <shopxnorthhub@gmail.com>
2025-04-29 17:28:13 +0200

    debug: dump client_key

--- automation_models.py
+++ automation_models.py
@@ -9,9 +9,10 @@ class ScheduledPost(db.Model):
     time = db.Column(db.Time, nullable=False)
     topic = db.Column(db.String(200), nullable=False)
     description = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    # `user_id` trzymamy jako string, bo przechowujemy TikTok open_id (alfanumeryczny)
+    user_id = db.Column(db.String(64), nullable=False)
     
-    def __init__(self, date, time, topic, description, user_id):
+    def __init__(self, date: date, time: time, topic: str, description: str, user_id: str):
         self.date = date
         self.time = time
         self.topic = topic
